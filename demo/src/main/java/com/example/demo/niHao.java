31c754061b6494b53e590aa5b15b765cc732c80c
HZZXHN <48684687+HZZXHN@users.noreply.github.com>
2020-12-13 21:05:21 +0800

    Update niHao.java

--- demo/src/main/java/com/example/demo/niHao.java
+++ demo/src/main/java/com/example/demo/niHao.java
@@ -5,5 +5,9 @@ public class niHao {
     public void delete(){
         System.out.println("taem2 开发了删除  第一天");
     }
+    
+    public void night(){
+        System.out.println("taem2 开发了夜嗨 深夜");
+    }
 
 }
